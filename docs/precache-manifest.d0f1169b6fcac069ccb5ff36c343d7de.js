715dbebfc66dd6d88f56d23eafa2fb7018ffb89e
Andreas Karel Völker <andreaskvolker@gmail.com>
2020-08-05 12:04:39 -0300

    deploy

---
+++ docs/precache-manifest.d0f1169b6fcac069ccb5ff36c343d7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105eeec96f9ac2cfb68e602bc1b75286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a82d5f82b18bf464a0dd",
+    "url": "/static/css/main.357a53db.chunk.css"
+  },
+  {
+    "revision": "ef4997a0a1fbb8c0bd12",
+    "url": "/static/js/2.34e4871e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.34e4871e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82d5f82b18bf464a0dd",
+    "url": "/static/js/main.0a3c1c03.chunk.js"
+  },
+  {
+    "revision": "94e40a7fa053a244e10d",
+    "url": "/static/js/runtime-main.d8a0e61c.js"
+  }
+]);
